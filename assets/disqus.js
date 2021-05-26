ba3307b1b6d5a5ac297de0cfbab8d23047311fd4
Navan Chauhan <navanchauhan@gmail.com>
2021-05-26 23:56:46 +0530

    removing old

--- assets/disqus.js
+++
@@ -1,5 +0,0 @@
-(function() {
-    var t = document,
-        e = t.createElement("script");
-    e.src = "https://navan-chauhan.disqus.com/embed.js", e.setAttribute("data-timestamp", +new Date), (t.head || t.body).appendChild(e)
-})();

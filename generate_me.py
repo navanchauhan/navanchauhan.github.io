04e60212d72e6a192d812fc3b150b805d03f4d63
Navan Chauhan <navanchauhan@gmail.com>
2026-07-05 22:37:51 -0700

    Remove custom text selection highlighting

--- generate_me.py
+++ generate_me.py
@@ -21,7 +21,6 @@ templates = Environment(loader=FileSystemLoader("templates"))
 VERSIONED_ASSET_PATHS = [
     "Resources/assets/c-hyde.css",
     "Resources/assets/main.css",
-    "Resources/assets/true-highlight.js",
     "Resources/manifest.json",
     "Resources/pwabuilder-sw-register.js",
     "Resources/pwabuilder-sw.js",
